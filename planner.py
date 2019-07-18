902e26d9ad69f1ed266a58f7122685168aff8fa4
EC2 Default User <ec2-user@ip-172-31-25-170.us-east-2.compute.internal>
2019-07-18 19:19:47 +0000

    priority done

--- planner.py
+++ planner.py
@@ -5,7 +5,7 @@ def questionList():
     what = input("Please tell us what task you would like to do today!\n")
     priority = input("How important is this task to you on a scale of 1-5?\n")
     time = input("How long do you estimate this task will take you in minutes?\n")
-    return [{"task": what, "priority": priority, "length": time}]
+    return {"task": what, "priority": priority, "length": time}
 
 
 userTask = []
@@ -23,12 +23,13 @@ while start:
         else:
             print("Here is your task list!")
             continueAsk = False
-            
+                
+    prioritized = sorted(userTask, key = lambda i: i["priority"], reverse = True)
     
     count = 0
-    for duty in userTask:  
+    for duty in prioritized:  
         i = 0
-        print(str(count+1) + ". " + duty[i]["task"].title()  + " for " + str(duty[i]["length"]) + " minutes.")
+        print(str(count+1) + ". " + duty["task"].title()  + " for " + str(duty["length"]) + " minutes.")
         count += 1
     start = False
     
