184bcfecbad565dd78587f6b08ede33d70c3f083
EC2 Default User <ec2-user@ip-172-31-25-170.us-east-2.compute.internal>
2019-07-18 18:28:42 +0000

    initial ommit

---
+++ planner.py
@@ -0,0 +1,34 @@
+#Initial interaction with user
+print("Hello! Welcome to the Schedule Planner 2000!")
+
+def questionList():
+    what = input("Please tell us what task you would like to do today!\n")
+    priority = input("How important is this task to you on a scale of 1-5?\n")
+    time = input("How long do you estimate this task will take you in minutes?\n")
+    return [{"task": what, "priority": priority, "length": time}]
+
+
+userTask = []
+priortized = []
+
+start = True
+while start:
+    userTask.append(questionList())
+    
+    continueAsk = True
+    while continueAsk:
+        askUser = input("Would you like to add anything to your list? Yes or No?\n")
+        if askUser.lower() == "yes":
+            userTask.append(questionList())
+        else:
+            print("Here is your task list!")
+            continueAsk = False
+            
+    
+    count = 0
+    for duty in userTask:  
+        i = 0
+        print(str(count+1) + ". " + duty[i]["task"].title()  + " for " + str(duty[i]["length"]) + " minutes.")
+        count += 1
+    start = False
+    
